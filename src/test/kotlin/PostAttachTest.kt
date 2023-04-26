214844273b73f5dd911ccb21f1d0963245f6b02c
Your Name <joy@example.com>
2023-04-27 00:01:29 +0300

    init

---
+++ src/test/kotlin/PostAttachTest.kt
@@ -0,0 +1,150 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class PostAttachTest {
+
+    @Test
+    fun testEquals() {
+    }
+
+    @Test
+    fun testHashCode() {
+    }
+
+    @Test
+    fun getOwnerId() {
+    }
+
+    @Test
+    fun getFromId() {
+    }
+
+    @Test
+    fun getCreatedBy() {
+    }
+
+    @Test
+    fun getAttachments() {
+    }
+
+    @Test
+    fun getOriginal() {
+    }
+
+    @Test
+    fun getText() {
+    }
+
+    @Test
+    fun getFriendsOnly() {
+    }
+
+    @Test
+    fun getLikes() {
+    }
+
+    @Test
+    fun getReposts() {
+    }
+
+    @Test
+    fun getPostType() {
+    }
+
+    @Test
+    fun getCanPin() {
+    }
+
+    @Test
+    fun getCanDelete() {
+    }
+
+    @Test
+    fun getCanEdit() {
+    }
+
+    @Test
+    fun isPinned() {
+    }
+
+    @Test
+    fun isFavorite() {
+    }
+
+    @Test
+    fun getId() {
+    }
+
+    @Test
+    operator fun component1() {
+    }
+
+    @Test
+    operator fun component2() {
+    }
+
+    @Test
+    operator fun component3() {
+    }
+
+    @Test
+    operator fun component4() {
+    }
+
+    @Test
+    operator fun component5() {
+    }
+
+    @Test
+    operator fun component6() {
+    }
+
+    @Test
+    operator fun component7() {
+    }
+
+    @Test
+    operator fun component8() {
+    }
+
+    @Test
+    operator fun component9() {
+    }
+
+    @Test
+    operator fun component10() {
+    }
+
+    @Test
+    operator fun component11() {
+    }
+
+    @Test
+    operator fun component12() {
+    }
+
+    @Test
+    operator fun component13() {
+    }
+
+    @Test
+    operator fun component14() {
+    }
+
+    @Test
+    operator fun component15() {
+    }
+
+    @Test
+    operator fun component16() {
+    }
+
+    @Test
+    fun copy() {
+    }
+
+    @Test
+    fun testToString() {
+    }
+}
--- src/test/kotlin/WallServiceTest1.kt
+++ src/test/kotlin/WallServiceTest1.kt
@@ -1,62 +1,26 @@
-import junit.framework.TestCase.assertEquals
-import org.junit.*
-import kotlin.test.assertFalse
+import org.junit.Test
+
+import org.junit.Assert.*
 
 class WallServiceTest {
-    @Before
-    fun clearBeforeTest() {
-        WallService.clear()
+
+    @Test
+    fun addNoZero() {
+        val post = WallService.add(Post(1, " "," ",))
+        assertNotEquals(0, post.id)
     }
 
     @Test
-    fun updateExisting() {
-        val post = Post(1, "Hello modified")
-        WallService.update(post)
+    fun updateExists() {
+        val post = WallService.add(Post(0L, 0L, 0L, 0L, 0L, 0L))
+        val exists = WallService.update(post.copy(text = "another text"))
+        assert(exists)
     }
 
     @Test
-    fun testAdd() {
-        val post = Post(1,"Post text added")
-        val addedPost = WallService.add(post)
-        assertEquals(post.text, addedPost.text)
-        assertEquals(1, addedPost.id)
-    }
-
-    @Test
-    fun testUpdate() {
-        val post = Post(id = 1, text = "Test Post")
-        WallService.add(post)
-
-        val updatedPost = post.copy(text = "Updated Test Post")
-        val result = WallService.update(updatedPost)
-        assertEquals(false, result)
-    }
-
-    @Test
-    fun testUpdate_notFound() {
-        val post = Post(id = 1, text = "Test ")
-        WallService.add(post)
-        val updatedPost = post.copy(id = 2)
-        val result = WallService.update(updatedPost)
-        assertFalse(result)
-    }
-
-
-    @Test
-    fun testPrintAll() {
-        val post1 = Post(id = 1, text = "Test Post 1")
-        val post2 = Post(id = 2, text = "Test Post 2")
-        WallService.add(post1)
-        WallService.add(post2)
-        WallService.printAll()
-    }
-
-    @Test
-    fun testMain() {
-        val post = Post(id = 1, text = "Test Post")
-        WallService.add(post)
-        WallService.add(post)
-        WallService.update(Post(id = 2, text = "Updated Test Post"))
-        main()
+    fun updateNotExists() {
+        val post = WallService.add(Post(0L, 0L, 0L, 0L, 0L, 0L))
+        val exists = WallService.update(post.copy(text = "another text", id = post.id + 1))
+        assert(!exists)
     }
 }
